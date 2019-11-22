9844d065507c2bb2b46ac5b5912504c2e4771873
Evan-CSE <53369789+Evan-CSE@users.noreply.github.com>
2019-11-22 17:54:44 +0600

    Watermelon

---
+++ codeforces 4A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(false);;cin.tie(NULL);cout.tie(NULL);
+/**Sum of even numbers will always be even number*/
+using namespace std;
+int main()
+{
+    fastio;
+    int w;
+    cin>>w;
+    if(w&1)
+        cout<<"NO"<<"\n";
+    else
+        cout<<"YES"<<"\n";
+   return 0;
+}
+//Evan
